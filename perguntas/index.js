9dcd23cdb30961691c711ed4ded4c45018d280f3
Robson Santos <robson.niai@gmail.com>
2022-09-18 12:20:27 -0300

    Exercicio 33

--- perguntas/index.js
+++ perguntas/index.js
@@ -9,6 +9,20 @@ da pasta views
 */
 app.set('view engine', 'ejs');
 
+/*Adicionamento pasta para arquivos estáticos  
+ */
+app.use(express.static('public'));
+
+//Rota para trabalhar com bootstrap
+app.get("/boot", function(req, res){
+    res.render("boot");
+})
+
+//Rota de perguntas
+app.get("/perguntar",function(req, res){
+    res.render("perguntar");
+})
+
 //Rota padrão para teste redenrizando .ejs
 //Não precisa colocar a extensão .ejs
 //Mandando variaveis para o HTML
@@ -16,14 +30,22 @@ app.set('view engine', 'ejs');
 app.get("/:nome?/:lang?", function(req, res){
     var nome = req.params.nome;
     var lang = req.params.lang;
+    var exibirMsg = true;
+    var produtos=[
+        {nome:"Doritos", preco:3.14},
+        {nome:"Coca-cola", preco:5.0},
+        {nome:"Leite", preco:1.45}
+    ]
+
     res.render("index",{
         nome: nome,
         lang: lang,
         empresa: "Guia do programador",
-        inscrito: 8000
+        inscrito: 8000,
+        msg: exibirMsg,
+        produtos: produtos
     });
 });
-
 
 //Inicia o servidor
 app.listen(8080, function(erro){
